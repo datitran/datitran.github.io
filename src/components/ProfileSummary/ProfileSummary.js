152e37c66fdd27a7d489adcd696eec9501a02d55
Dat Tran <datitran@gmail.com>
2019-02-06 10:18:31 +0100

    Optimize profile summary for mobile.

--- src/components/ProfileSummary/ProfileSummary.js
+++ src/components/ProfileSummary/ProfileSummary.js
@@ -7,7 +7,7 @@ class ProfileSummary extends React.Component {
   render() {
     return <Jumbotron>
       <Container className='profilesummary__container'>
-        <h1 className='display-4'>{profile.title}</h1>
+        <h1 className='profilesummary__display_4'>{profile.title}</h1>
         <p className='profilesummary__lead'>{profile.summary}</p>
       </Container>
     </Jumbotron>;
