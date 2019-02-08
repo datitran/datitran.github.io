d1cc63a89fa1705aaf1ddf59a148eafface11589
Dat Tran <datitran@gmail.com>
2019-02-08 16:56:20 +0100

    Add more project examples on main page.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Container, Row, Col, CardDeck, Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import './Home.scss';
 import imageOne from '../../static/object_detection.jpg';
 import imageTwo from '../../static/face2face.jpg';
+import imageThree from '../../static/handwritten_digit_recognition.gif';
+import imageFour from '../../static/raccoon_detector.jpg';
 
 class Home extends React.Component {
   render() {
@@ -24,6 +27,24 @@ class Home extends React.Component {
               <a href='https://towardsdatascience.com/face2face-a-pix2pix-demo-that-mimics-the-facial-expression-of-the-german-chancellor-b6771d65bf66'><CardImg src={imageTwo} alt='Face2face Demo.'/></a>
               <CardBody>
                 <CardTitle>Face2face — A Pix2Pix demo that mimics the facial expression of the German chancellor.</CardTitle>
+              </CardBody>
+            </Card>
+          </CardDeck>
+        </Col>
+      </Row>
+      <Row className='home__row'>
+        <Col>
+          <CardDeck>
+            <Card>
+              <a href='http://engineering.pivotal.io/post/api-first-for-data-science/'><CardImg src={imageThree} alt='Handwritten Digit Recognition.'/></a>
+              <CardBody>
+                <CardTitle>Handwritten Digit Recognition Demo.</CardTitle>
+              </CardBody>
+            </Card>
+            <Card>
+              <a href='https://towardsdatascience.com/how-to-train-your-own-object-detector-with-tensorflows-object-detector-api-bec72ecfe1d9'><CardImg src={imageFour} alt='Raccoon Detector.'/></a>
+              <CardBody>
+                <CardTitle>Using TensorFlow’s Object Detector API to create a Raccoon Detector.</CardTitle>
               </CardBody>
             </Card>
           </CardDeck>
