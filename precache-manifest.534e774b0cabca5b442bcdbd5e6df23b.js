82160b1a93f0147b260bca5ce5f75bff62cf23b9
datitran <datitran@users.noreply.github.com>
2024-02-29 14:45:47 +0000

    Deploying to gh-pages from @ datitran/datitran.github.io@b7a52d6d3e63d0cfca1ca3feab749914b4eedf81 🚀

---
+++ precache-manifest.534e774b0cabca5b442bcdbd5e6df23b.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14da6a7622c4cef40bdce778ec7d8b29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de715ed179a0e58abbed",
+    "url": "/static/css/2.4e15b5ff.chunk.css"
+  },
+  {
+    "revision": "89dc7f4dfdf2e60a8045",
+    "url": "/static/css/main.0c77c6e5.chunk.css"
+  },
+  {
+    "revision": "de715ed179a0e58abbed",
+    "url": "/static/js/2.3ca5d1cd.chunk.js"
+  },
+  {
+    "revision": "8499a0961a0507e5f6c9eff69ac126aa",
+    "url": "/static/js/2.3ca5d1cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89dc7f4dfdf2e60a8045",
+    "url": "/static/js/main.ca43207a.chunk.js"
+  },
+  {
+    "revision": "44c05a02fe4e62c8681f",
+    "url": "/static/js/runtime-main.bff4bee0.js"
+  },
+  {
+    "revision": "fea2e2ac168396372880f5dcff3f37f5",
+    "url": "/static/media/CV.fea2e2ac.pdf"
+  },
+  {
+    "revision": "4e3d35da146ab8e8b9913e348f230b8e",
+    "url": "/static/media/apache_nifi.4e3d35da.jpg"
+  },
+  {
+    "revision": "67f99b7ccb9a9c6d32c184edf910c4c7",
+    "url": "/static/media/employer_branding_dat_tran.67f99b7c.jpg"
+  },
+  {
+    "revision": "a36b9d9616bb36d43b3ae91d145fdb8b",
+    "url": "/static/media/face2face.a36b9d96.jpg"
+  },
+  {
+    "revision": "4344b72a1742fa87aa333bf16b9e129c",
+    "url": "/static/media/face2face_tn.4344b72a.jpg"
+  },
+  {
+    "revision": "299d1763cd29c2daf6193c30ca7375cf",
+    "url": "/static/media/filter_visualization_aesthetic.299d1763.jpg"
+  },
+  {
+    "revision": "b2425a2a4b224881bc160bc6deb09d1e",
+    "url": "/static/media/filter_visualization_aesthetic_tn.b2425a2a.jpg"
+  },
+  {
+    "revision": "2df6f7ba229bf2768c93c16025eb5877",
+    "url": "/static/media/handwritten_digit_recognition.2df6f7ba.gif"
+  },
+  {
+    "revision": "9949cb84cfc8d7e33e5bfba82be67130",
+    "url": "/static/media/header.9949cb84.jpg"
+  },
+  {
+    "revision": "e6acf34db103136ba1a032c2f170b08a",
+    "url": "/static/media/idealo_hotel.e6acf34d.jpg"
+  },
+  {
+    "revision": "3c01b49d3f15e80c39b51df0fe9c6fd9",
+    "url": "/static/media/idealo_team_photo.3c01b49d.jpg"
+  },
+  {
+    "revision": "83578f2e7b12e7369604395c3197b790",
+    "url": "/static/media/jupyter_reveal.83578f2e.gif"
+  },
+  {
+    "revision": "524da79af6dde91f4810f666b401f75e",
+    "url": "/static/media/leadership_dat_tran.524da79a.jpg"
+  },
+  {
+    "revision": "35d6f0f779865b48eff73b3b8970b1f8",
+    "url": "/static/media/logo_actionable_data.35d6f0f7.png"
+  },
+  {
+    "revision": "ed376d2046c9ee5531c4bfd743e0a863",
+    "url": "/static/media/logo_alldus.ed376d20.jpg"
+  },
+  {
+    "revision": "caddfc0001548e9397c5352ee4e807ae",
+    "url": "/static/media/logo_aws.caddfc00.jpg"
+  },
+  {
+    "revision": "59805dca50d76f6c406b5fa5c2c17e14",
+    "url": "/static/media/logo_cloudfoundry.59805dca.png"
+  },
+  {
+    "revision": "93e453005b2fce158c6cf7adab283cbd",
+    "url": "/static/media/logo_datafestival.93e45300.jpg"
+  },
+  {
+    "revision": "baf7f7426ce5eb2346856d649355aa13",
+    "url": "/static/media/logo_datahack.baf7f742.jpg"
+  },
+  {
+    "revision": "4f13ee18112994a1bf2aacd27687a88b",
+    "url": "/static/media/logo_dataiku.4f13ee18.jpg"
+  },
+  {
+    "revision": "bdfefca9a1fe65d33e2b355ed89d98ab",
+    "url": "/static/media/logo_datanatives.bdfefca9.jpg"
+  },
+  {
+    "revision": "cdc33f6c6383035d1a234059d5c96adb",
+    "url": "/static/media/logo_digitaleleute.cdc33f6c.jpg"
+  },
+  {
+    "revision": "8568b7a92e21b78452d2427bf1d21196",
+    "url": "/static/media/logo_freecodecamp.8568b7a9.png"
+  },
+  {
+    "revision": "674168aa204792ee69e6b2fd2ba953a0",
+    "url": "/static/media/logo_hub.674168aa.jpg"
+  },
+  {
+    "revision": "5fcfbe5fab8888143de265d25632f53f",
+    "url": "/static/media/logo_idealotech.5fcfbe5f.jpg"
+  },
+  {
+    "revision": "8abf28d760821a2ec6738571a9784bc0",
+    "url": "/static/media/logo_iect.8abf28d7.jpg"
+  },
+  {
+    "revision": "dca786f82425dca13a6d49a8e9575000",
+    "url": "/static/media/logo_meetup.dca786f8.png"
+  },
+  {
+    "revision": "14ddc469f203e3f3751fc8171c8db7bb",
+    "url": "/static/media/logo_mindsnack.14ddc469.jpg"
+  },
+  {
+    "revision": "61de0b9afe362548b44d4bfc0dbdf58b",
+    "url": "/static/media/logo_nvidia.61de0b9a.png"
+  },
+  {
+    "revision": "d1ac2614d90b03c91653be19065b4d26",
+    "url": "/static/media/logo_odsc.d1ac2614.png"
+  },
+  {
+    "revision": "0ad1bf7e24790963fa357262bd817e70",
+    "url": "/static/media/logo_pydata.0ad1bf7e.png"
+  },
+  {
+    "revision": "2c22510a80370e3c52693d05636bf450",
+    "url": "/static/media/logo_speedchangerepeat.2c22510a.jpg"
+  },
+  {
+    "revision": "413fcdf275743ec4a66bed1c617005f5",
+    "url": "/static/media/logo_towards_data_science.413fcdf2.jpg"
+  },
+  {
+    "revision": "f8e99ef4120466bdc81c8b5b00e14a04",
+    "url": "/static/media/logo_venturi.f8e99ef4.jpg"
+  },
+  {
+    "revision": "034c0416a1812d37a92a90b2858e55d3",
+    "url": "/static/media/logo_wearedevelopers.034c0416.jpg"
+  },
+  {
+    "revision": "7fa7e3a74fe5b2654a339d64f91c1012",
+    "url": "/static/media/object_detection.7fa7e3a7.jpg"
+  },
+  {
+    "revision": "4d434e4e9865ca6a040df1823e128588",
+    "url": "/static/media/object_detection_tn.4d434e4e.jpg"
+  },
+  {
+    "revision": "9bfff674a83aa6d5be815edf4567bb18",
+    "url": "/static/media/raccoon_detector.9bfff674.jpg"
+  },
+  {
+    "revision": "6a1f014eeb91f191d8702e2d54189d85",
+    "url": "/static/media/readaloud_welt.6a1f014e.jpg"
+  }
+]);
