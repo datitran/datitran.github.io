03af7c7c22107f4663add6b8e1eadc21ec1f0bb8
Dat Tran <datitran@gmail.com>
2024-03-01 11:50:45 +0100

    revert

--- src/services/GitHubService.js
+++ src/services/GitHubService.js
@@ -2,16 +2,11 @@ import HttpService from './HttpService';
 
 class GitHubService {
     constructor() {
-        this.BASE_URL = 'https://api.github.com/repos';
+        this.BASE_URL = 'https://asideas-github-cache.herokuapp.com';
     }
 
     getStats(owner, project) {
-        const url = `${this.BASE_URL}/${owner}/${project}`;
-        return HttpService.get(url, {
-            headers: {
-                'Accept': 'application/vnd.github.v3+json'
-            }
-        });
+        return HttpService.get(this.BASE_URL + '/' + owner + '/' + project);
     }
 }
 
