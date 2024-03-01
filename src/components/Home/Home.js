a90817fe3cc1d279558618548bd7379cfbea0b8b
Dat Tran <datitran@gmail.com>
2024-03-01 12:41:05 +0100

    update home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -23,6 +23,14 @@ class Home extends React.Component {
       This is my personal homepage. I work in the intersection between humans and machines. Feel free to contact me at <a href="mailto:hello@dat-tran.com" className='home__links'>hello@dat-tran.com</a> for work-related inquiries.
       <br></br>
       <br></br>
+      <p>I offer services in:</p>
+      <ul>
+        <li>Tech due diligence: 🔤 I check code quality, do architectural review, review your tech stack and roadmap</li>
+        <li>Keynote talks: 🎙️ Introductory AI talks for both technical and non-technical audience, topics range from Generative AI to old boring ML</li>
+        <li>Discovery and strategy workshops: 🧭 I help you explore AI use cases tailored to your needs and rank them according to business value</li>
+        <li>Sparring Partner: 🤖 Due to my wide range of experiences, I can be a sparring partner for your team for both higher level management and also working level</li>
+      </ul>
+      <br></br>
       <Row>
         <Col>
           <CardDeck>
